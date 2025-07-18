6e55275e970f94b6aed933c5b0e2f207e9038c94
connorknoetze <connorknoetze@gmail.com>
2025-07-19 07:45:40 +1200

    added sizes and process different sizes between clients.

--- client_launcher.py
+++ client_launcher.py
@@ -5,15 +5,14 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 filepath = os.path.join(base_dir, "assets", "downloaded.txt")
 file = open(filepath,"r")
 _,b = file.readline().strip().split("=", 1)
-file.close
+file.close()
 
 if b == "False":
     subprocess.Popen(["start", "cmd", "/k", "pip install pygame"], shell=True)
     file = open(filepath,"w")
     file.write("downloaded=True")
+    file.close()
 else:
     subprocess.Popen(["start", "cmd", "/k", "python server.py"], shell=True)
     for i in range(2): # change range for number of clients you want to test with
         subprocess.Popen(["start", "cmd", "/k", "python main.py"] , shell=True)
-
-file.close()
--- main.py
+++ main.py
@@ -10,17 +10,21 @@ py.display.set_caption("Paint Pygame")
 clock = py.time.Clock()
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
-color_assets_dir_off = os.path.join(base_dir, "assets", "colors","off")
-color_assets_dir_on = os.path.join(base_dir, "assets", "colors", "on")
-color_assets_off = [os.path.join(color_assets_dir_off, file) for file in os.listdir(color_assets_dir_off)]
-color_assets_on = [os.path.join(color_assets_dir_on, file) for file in os.listdir(color_assets_dir_on)]
-color_selected = [True]+[False for _ in color_assets_off][:-1]
-
 tool_assets_dir_off = os.path.join(base_dir, "assets", "tools","off")
 tool_assets_dir_on = os.path.join(base_dir, "assets", "tools", "on")
 tool_assets_off = [os.path.join(tool_assets_dir_off, file) for file in os.listdir(tool_assets_dir_off)]
 tool_assets_on = [os.path.join(tool_assets_dir_on, file) for file in os.listdir(tool_assets_dir_on)]
 tool_selected = [True]+[False for _ in tool_assets_on][:-1]
+
+color_assets_dir_off = os.path.join(base_dir, "assets", "colors","off")
+color_assets_dir_on = os.path.join(base_dir, "assets", "colors", "on")
+color_assets_off = [os.path.join(color_assets_dir_off, file) for file in os.listdir(color_assets_dir_off)]
+color_assets_on = [os.path.join(color_assets_dir_on, file) for file in os.listdir(color_assets_dir_on)]
+
+size_assets_dir_off = os.path.join(base_dir, "assets", "size","off")
+size_assets_dir_on = os.path.join(base_dir, "assets", "size", "on")
+size_assets_off = [os.path.join(size_assets_dir_off, file) for file in os.listdir(size_assets_dir_off)]
+size_assets_on = [os.path.join(size_assets_dir_on, file) for file in os.listdir(size_assets_dir_on)]
 
 
 class Toolbar():
@@ -128,12 +132,68 @@ class Colors():
             if rect.collidepoint(mouse_pos):
                 if self.color_index != None and self.color_index != i:
                     self.swap(self.color_index)
-                    color_selected[self.color_index] = False
                     self.color_index = None
 
                     self.swap(i)
                     self.color_index = i
-                    color_selected[i] = True
+
+class Sizes():
+    def __init__(self):
+        five = 5
+        seven = 7
+        ten = 10
+        twelve = 12
+
+        self.sizes = [five, seven, ten, twelve]
+
+        gap = 41
+        size = 32
+        xstart = 1009
+        ystart = 41*len(color_assets_off) * 2
+        image_start = ystart + 9
+        self.end = image_start+ len(size_assets_off)*gap
+
+        self.swapped = False
+        self.rect = py.Rect(1000, ystart, 50, 41 * len(color_assets_off))
+        self.size_index = 0
+
+        self.assets_rects = [py.Rect(xstart, y, size, size) for y in range(image_start, self.end, gap)] 
+        self.images = [py.transform.scale(py.image.load(filepath).convert_alpha(), (size, size)) for filepath in size_assets_off]
+        self.swap(0)
+
+    def get_end(self):
+        return self.end
+    
+    def get_size(self):
+        return self.sizes[self.size_index]
+
+    def draw(self):
+        py.draw.rect(screen, (100,100,100), self.rect)
+        [screen.blit(self.images[i], self.assets_rects[i]) for i in range(len(self.assets_rects))]
+
+    def get_rects(self):
+        return self.assets_rects
+
+    def swap(self, index):
+        if self.swapped:
+            path = os.path.join(base_dir, size_assets_off[index])
+        else:
+            path = os.path.join(base_dir, size_assets_on[index])
+        
+        self.images[index] = py.transform.scale(py.image.load(path).convert_alpha(), (32,32))
+        self.swapped = not self.swapped
+
+    def update(self, mouse_pos):
+        rects = self.get_rects()
+        for i in range(len(rects)):
+            rect = rects[i]
+            if rect.collidepoint(mouse_pos):
+                if self.size_index != None and self.size_index != i:
+                    self.swap(self.size_index)
+                    self.size_index = None
+
+                    self.swap(i)
+                    self.size_index = i
 
 
 class Canvas:
@@ -141,15 +201,19 @@ class Canvas:
         self.surface = py.Surface((width, height))
         self.surface.fill((255, 255, 255))
         self.brush_color = (0, 0, 0)
+        self.size = 5
 
     def draw_message(self, color, x, y, size=5):
         py.draw.circle(self.surface, color, (x, y), size)
 
-    def draw_brush(self, x, y, size=5):
-        py.draw.circle(self.surface, self.brush_color, (x, y), size)
+    def draw_brush(self, x, y):
+        py.draw.circle(self.surface, self.brush_color, (x, y), self.size)
 
     def assign_color(self, color):
         self.brush_color = color
+    
+    def assign_size(self, size):
+        self.size = size
 
     def draw_square(self, points, message_points):
         x1,y1 = points[0]
@@ -240,13 +304,14 @@ class Display():
     def update(self, mouse_pos, message_points):
         if tool_selected[0] and 0<= mouse_pos[0] < self.size and 0<= mouse_pos[1] < self.size:
             message_points.append(mouse_pos)
-            self.canvas.draw_brush(mouse_pos[0], mouse_pos[1], size=5)
+            self.canvas.draw_brush(mouse_pos[0], mouse_pos[1])
 
 
 def main():
     display = Display()
     toolbar = Toolbar()
     colors = Colors()
+    sizes = Sizes()
 
     running = True
     pressed = False
@@ -278,11 +343,11 @@ def main():
         with lock:
             if recv_messages:
                 temp = recv_messages[0].split("//")
-                color, message = temp[0], temp[1]
+                size, color, message = temp[0], temp[1], temp[2]
                 color = color.split(",")
                 color = int(color[0]), int(color[1]), int(color[2])
                 points = [point.split(",") for point in message.split("/")[:-1]]
-                [display.canvas.draw_message(color, int(point[0]), int(point[1])) for point in points]
+                [display.canvas.draw_message(color, int(point[0]), int(point[1]), size=int(size)) for point in points]
                 recv_messages.clear()
         
         for event in py.event.get():
@@ -298,6 +363,9 @@ def main():
                 elif 1000 <= mouse_pos[0] < 1050 and toolbar.get_end() <= mouse_pos[1] < colors.get_end():
                     colors.update(mouse_pos)
                     display.canvas.assign_color(colors.get_color())
+                elif 1000 <= mouse_pos[0] < 1050 and colors.get_end() <= mouse_pos[1] < sizes.get_end():
+                    sizes.update(mouse_pos)
+                    display.canvas.assign_size(sizes.get_size())
                         
             elif event.type == py.MOUSEBUTTONUP:
                 pressed = False
@@ -320,12 +388,13 @@ def main():
         else:
             if message_points:
                 color = display.canvas.brush_color
-                Client.send_message(f"{color[0]},{color[1]},{color[2]}//"+"".join([f"{m[0]},{m[1]}/"for m in set(message_points)]), sock)
+                Client.send_message(f"{sizes.get_size()}//"+f"{color[0]},{color[1]},{color[2]}//"+"".join([f"{m[0]},{m[1]}/"for m in set(message_points)]), sock)
             message_points = []
 
         display.draw()
         toolbar.draw()
         colors.draw()
+        sizes.draw()
 
         clock.tick(500)
         py.display.flip()
